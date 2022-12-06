0ae6beb8ff13f1975d80486eff04b5ba4eaa69eb
Baramex <contact@baramex.me>
2022-12-06 19:03:01 +0100

    improved login page

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,3 +1,4 @@
+import { UserCircleIcon } from "@heroicons/react/24/solid";
 import Footer from "../Layout/Footer";
 import Header from "../Layout/Header";
 import { Button } from "../Misc/Buttons";
@@ -7,7 +8,8 @@ export default function Login(props) {
     return (<>
         <Header {...props} />
         <div className="pt-[4.5rem] bg-theme-700 text-white pb-12 px-6">
-            <h1 className="text-center text-4xl font-medium my-20">Espace Employé - Connexion</h1>
+            <UserCircleIcon className="text-gray-100 mx-auto w-16 mt-11 mb-8" />
+            <h1 className="text-center text-4xl font-medium mb-14">Espace Employé - Connexion</h1>
             <div className="px-6 max-w-xl mx-auto">
                 <form className="flex flex-col gap-6">
                     <TextField
