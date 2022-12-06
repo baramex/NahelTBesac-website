18e7fe1842cac210636b6f59a9e827b2b83998a7
Baramex <contact@baramex.me>
2022-12-06 18:55:40 +0100

    footer -> bottom

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,5 +1,5 @@
 export default function Footer() {
-    return (<div className="flex justify-center text-sm text-white mt-10 mb-2">
+    return (<div className="absolute bottom-0 w-full flex justify-center text-sm text-white pb-2 pt-5 bg-[#393d32]">
         <p>Tous droits réservés © 2022 par <a className="underline" target="_BLANK" rel="noreferrer" href="https://baramex.me">baramex</a> pour <span className="font-medium">Nahel Transport</span>.</p>
     </div>);
 }
