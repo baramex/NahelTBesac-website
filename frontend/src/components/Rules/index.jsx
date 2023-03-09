cc2407f88c08def59c280b8ef5463ca8e963bf58
Baramex <contact@baramex.me>
2023-03-09 20:46:52 +0100

    fautes

--- frontend/src/components/Rules/index.jsx
+++ frontend/src/components/Rules/index.jsx
@@ -10,16 +10,16 @@ export default function Rules(props) {
             <h1 className="text-center text-4xl font-medium mb-10">Règlement Nahel Transport</h1>
             <div className="px-6 flex justify-center">
                 <div className="max-w-5xl">
-                    <p className="text-lg">Le règlement suivante constitut un contrat légal entre les Livreurs et Nahel Transport définissant les règles et le comportement acceptable à adopter au dépôt et en livraison.</p><br /><br />
-                    <p>Bien évidemment pas besoin de détaillés ce qu'est un comportement acceptable.</p><br /><br />
-                    <p>Soyez Polis , Souriant et Courtois et tout devrais bien se passer.</p><br /><br />
-                    <p>A coté de ça vous devez savoir que certaines primes sont accordés en fonction de ce que vous faites.<br />
-                        Votre contrat ne spécifie pas une tournée en particulier , n'importe laquelle peux vous être attribuer.</p><br /><br />
+                    <p className="text-lg">Le règlement suivant constitue un contrat légal entre les Livreurs et Nahel Transport définissant les règles et le comportement acceptable à adopter au dépôt et en livraison.</p><br /><br />
+                    <p>Bien évidemment pas besoin de détailler ce qu'est un comportement acceptable.</p><br /><br />
+                    <p>Soyez Polis , Souriant et Courtois et tout devrait bien se passer.</p><br /><br />
+                    <p>A côté de ça vous devez savoir que certaines primes sont accordés en fonction de ce que vous faites.<br />
+                        Votre contrat ne spécifie pas une tournée en particulier , n'importe laquelle peut vous être attribuée.</p><br /><br />
                     <div>
                         <h2 className="text-lg font-medium mb-4">Vous pouvez facilement gagner votre prime en :</h2>
                         <ul className="list-disc ml-5 [&>*]:my-1">
                             <li>Etant à L'heure au dépôt : 8h</li>
-                            <li>Envoyer les photo de vos colis le matin</li>
+                            <li>Envoyer les photos de vos colis le matin</li>
                             <li>Remplir le formulaire le soir</li>
                             <li>Avoir un Camion Propre</li>
                             <li>Conduire prudemment et éviter les accidents</li>
